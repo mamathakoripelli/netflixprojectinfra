1b02773b9ec0f882bdd8fff4da299af23009b183
mamatha koripelli <Koripellimamatha@gmail.com>
2024-12-04 11:38:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
+count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
-key_name = "rahull"
-vpc_security_groups_ids = ["sg-05f044979e305302e"]
+key_name = "mamathakp"
+vpc_security_group_ids = ["sg-02768082ea8db393e"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2", monitoring"]
 }
