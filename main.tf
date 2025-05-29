bebc8f01b39eb89af72b587923187c92784b5a55
mamatha koripelli <Koripellimamatha@gmail.com>
2025-05-29 12:23:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-0953476d60561c955"
 instance_type = "t2.medium"
-key_name = "rahull"
-vpc_security_groups_ids = ["sg-05f044979e305302e"]
+key_name = "mkp"
+vpc_security_groups_ids = ["sg-01150710c0d4fc0d9"]
 tags = {
 Name = var.instance_names[count.index]
 }
